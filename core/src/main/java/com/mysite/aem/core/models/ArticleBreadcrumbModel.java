ef13f4194bc70b45e9e94357198a42c8a0c51575
Mat-lax <laxmanpeddi915@gmail.com>
2026-04-06 18:29:36 +0530

    breadcrumb is working fine even though it is allowing from the xf

--- core/src/main/java/com/mysite/aem/core/models/ArticleBreadcrumbModel.java
+++ core/src/main/java/com/mysite/aem/core/models/ArticleBreadcrumbModel.java
@@ -1,22 +1,26 @@
 package com.mysite.aem.core.models;
 
 import com.day.cq.wcm.api.Page;
-import com.day.cq.wcm.api.PageManager;
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
-@Model(adaptables = SlingHttpServletRequest.class)
+@Model(
+    adaptables = SlingHttpServletRequest.class,
+    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
 public class ArticleBreadcrumbModel {
 
-    @SlingObject
-    private SlingHttpServletRequest request;
+    // This is the "magic" fix. It always grabs the page from the URL context,
+    // even if this component is rendered inside an Experience Fragment.
+    @ScriptVariable
+    private Page currentPage;
 
     private List<Page> breadcrumbs;
 
@@ -24,17 +28,19 @@ public class ArticleBreadcrumbModel {
     protected void init() {
         breadcrumbs = new ArrayList<>();
 
-        Page currentPage = Optional.ofNullable(request)
-                .map(req -> req.getResourceResolver().adaptTo(PageManager.class))
-                .flatMap(pageManager -> Optional.ofNullable(pageManager.getContainingPage(request.getResource())))
-                .orElse(null);
-
+        // No need for complex Optional chaining here! 
+        // AEM gives us the correct currentPage object via @ScriptVariable.
         if (currentPage != null) {
             Page tempPage = currentPage;
+            
             while (tempPage != null) {
+                // Logic check: Ensure we don't include the 'root' or 
+                // non-content nodes if your site structure requires it.
                 breadcrumbs.add(tempPage);
                 tempPage = tempPage.getParent();
             }
+            
+            // Reverse so it goes: Home > Category > Article
             Collections.reverse(breadcrumbs);
         }
     }
